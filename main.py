3bbf86855cdd9704c22e457295164ec4d20416f1
jasperntc <jasperntc@gmail.com>
2026-05-26 03:07:48 +0800

    Implemented roles to prompts sent to api for chat history. Added optional --verbose flag.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -13,19 +14,24 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="The prompt to send to the chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     model = "gemini-2.5-flash"
-    prompt = args.user_prompt
+    messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+]
 
-    response = client.models.generate_content(model=model, contents=prompt)
+    response = client.models.generate_content(model=model, contents=messages)
 
     if response.usage_metadata is None:
         raise RuntimeError("No usage metadata is found in the response")
 
-    print("User prompt: ", prompt)
-    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    if args.verbose:
+        print("User prompt: ", args.user_prompt)
+        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+        
     print("Response:")
     print(response.text)
 
